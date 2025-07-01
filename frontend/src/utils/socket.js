8fa0701718b1f6cd9cfe2929c964e7da1932b33c
Gaddala Varun <varunchintu2k3@gmail.com>
2025-07-01 13:14:12 +0530

    changes in frontend

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,4 +1,4 @@
 // src/utils/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("https://mindlink-backend-3v9l.onrender.com"); // Change if deploying
+export const socket = io("https://mindlink-nkdz.onrender.com"); // Change if deploying
